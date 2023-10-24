422f1f59e9d8ca2b69786dc9dd47b99a6e380ff6
Chen-Rong-Zi <1398881912@qq.com>
2023-10-24 23:21:27 +0800

    finish rref and augment feature

--- matrix.py
+++ matrix.py
@@ -1,10 +1,10 @@
 # encoding=UTF-8
 
-def is_argment(matr):
-    return hasattr(matr, '__call__') and matr("cls") == "Argment"
+def is_augment(matr):
+    return (hasattr(matr, '__call__') and (matr("cls") == "Augment") or matr('cls') == 'A')
 
 def is_matrix(matr):
-    return hasattr(matr, '__call__') and matr("cls") == 'matrix'
+    return (hasattr(matr, '__call__') and matr("cls") == 'matrix') or (hasattr(matr, '__call__') and (matr("cls") == "Augment") or matr('cls') == 'A')
 
 def make_matrix(*args):
     row, column = (args[0], args[0]) if len(args) == 1 else (args[0], args[1])
@@ -35,37 +35,42 @@ def matrix(*args, cls='matrix'):
 
         return matrix_function
 
-    if cls == "Augment":
+    if cls == "Augment" or cls == 'A':
         assert len(args) == 2 and is_matrix(args[0]) and is_matrix(args[1]), "only matrix can be added up to argument matrix"
         row_list = []
         for i in range(args[0]('row')):
             row_list.append(args[0](i) + args[1](i))
         augment = matrix(row_list)
 
-        def argment_function(x, y='noparameter', cls=cls):
+        def augment_function(x, y='noparameter', cls=cls):
+            if x == 'a':
+                return args[0]
+            if x == 'b':
+                return args[1]
             if x == 'cls':
                 return cls
             if x == 'row':
                 return args[0]('row')
             if x == 'column':
-                return args[1]('column')
+                return args[0]('column')
             if y == 'noparameter':
                 return augment(x)
             assert x >= 0 and y >= 0, "x, y must at least 0"
             return augment(x, y)
+        return augment_function
 
-
-def print_matrix(matrix):
-    assert is_matrix(matrix), "must be a matrix"
+def print_matrix(matr, end=''):
+    assert is_matrix(matr) or is_augment(matr), "must be a matrix"
 
     lenth_list = []
-    for j in range(matrix('column')):
-        lenth_list.append( max( [ len(str(i)) for i in columns(matrix)[j] ] ) )
+    for j in range(len(matr(0))):
+        lenth_list.append( max( [ len(str(i)) for i in [matr(k, j) for k in range(matr('row'))] ] ) )
 
-    for i in rows(matrix):
+    for i in rows(matr):
         print("(", end='')
         for j in range(len(i)):
-            print(str(i[j]).rjust(lenth_list[j]), end=", " if j != len(i)-1 else ')\n')
+            print(str(i[j]).rjust(lenth_list[j]), end=(" ⏐ " if j == matr('row') else ", ") if j != len(i)-1 else ')\n')
+    print(end, end='')
 
 def matrix_type(matrix):
     assert is_matrix(matrix), "must be a matrix" 
@@ -80,14 +85,14 @@ def matrix_column(matr):
     return matr('column')
 
 def rows(matr):
-    assert is_matrix(matr), "must be a matrix"
+    assert is_matrix(matr) or is_augment(matr), "must be a matrix"
     rows_list = []
     for i in range(matr('row')):
         rows_list.append(copy_list(matr(i)))
     return rows_list
 
 def columns(matr):
-    assert is_matrix(matr), "must be a matrix"
+    assert is_matrix(matr) or is_augment(matr), "must be a matrix"
     columns_list = []
     for column in range(matr('column')):
         columns_list.append([ matr(i, column) for i in range(matr('row')) ])
@@ -110,7 +115,7 @@ def mul_matrix(m1, m2):
         product = 0
         for i in range(len(list1)):
             product += list1[i] * list2[i]
-        return five_place(product)
+        return clean_product(product)
 
     result = []
     for i in range(m1('row')):
@@ -127,7 +132,7 @@ def kth_matrix(matr, k):
     original_rows = rows(matr)
     new_rows = []
     for j in range(matr('row')):
-        new_rows.append([five_place(k * i) for i in matr(j)])
+        new_rows.append([clean_product(k * i) for i in matr(j)])
     return matrix(new_rows)
 
 def pow_matrix(matr, n):
@@ -196,60 +201,89 @@ def zero_matrix(*args):
     lst = [0 for i in range(column)]
     return matrix([copy_list(lst) for i in range(row)])
 
+def make_E_row(n, k, b):
+    return [(0 if i != b else k) for i in range(n)]
+
 def identity_matrix(n, x=0, y=0):
-    assert x != y, "can't swap the same row"
     rows_list = []
     for i in range(n):
-        row = []
-        for j in range(n):
-            if (i == x and i == j) or (i == y and i == j):
-                row.append(0)
-                continue
-            if (i == x and j == y) or (i == y and j == x):
-                row.append(1)
-                continue
-            if j == i:
-                row.append(1)
-                continue
-            row.append(0)
-        rows_list.append(row)
+        if i == x:
+            rows_list.append(make_E_row(n, 1, y))
+        elif i == y:
+            rows_list.append(make_E_row(n, 1, x))
+        else :
+            rows_list.append(make_E_row(n, 1, i))
     return matrix(rows_list)
 
 def swap_row(matr, x, y):
     assert is_matrix(matr), "must be a matrix"
     E = identity_matrix(matrix_row(matr), x, y)
-    return mul_matrix(E, matr)
+    return mul_matrix(E, matr), E
 
 def swap_column(matr, x, y):
     assert is_matrix(matr), "must be a matrix"
     tran = transfer(matr)
     E = identity_matrix(matrix_row(tran), x, y)
-    return transfer(mul_matrix(E, tran))
+    return transfer(mul_matrix(E, tran)), E
 
-def five_place(x):
+def clean_product(x):
     if x == 0:
         return 0
     if x > 0:
         decimal = x % 1
-        return int(x) if (decimal) <= 1e-6 else int(x)+1 if (1-decimal) <= 1e6 else x
+        return int(x) if (decimal) <= 1e-6 else int(x)+1 if (1-decimal) <= 1e-6 else x
     else:
         decimal = (-x) % 1
-        return -int(x) if (decimal) <= 1e-6 else -(int(x)+1) if (1-decimal) <= 1e6 else -x
+        return -int(x) if (decimal) <= 1e-6 else -(int(x)+1) if (1-decimal) <= 1e-6 else -x
 
 def add_row(matr, k, x, y):
-    new_rows = []
-    temp_row = [i * k for i in matr(x)]
+    E_rows = []
     for i in range(matr('row')):
         if i == y:
-            new_rows.append([temp_row[i]+matr(y, i) for i in range(matr('column'))])
+            E_rows.append(make_E_row(matr('row'), k, x))
             continue
-        new_rows.append(copy_list(matr(i)))
-    return matrix(new_rows)
+        E_rows.append(make_E_row(matr('row'), 0, i))
+    E = add_matrix(matrix(E_rows), identity_matrix(matr('row')))
+    return mul_matrix(E, matr), E
 
-# def rref(matr):
-#     for i in range(matr('row')):
-#         for j in range(matr('column')):
-# 
+def rref(matr, display=False, operation=False):
+    assert is_matrix(matr), "must be a matrix to be applied rref to!"
+
+    if is_augment(matr):
+        a, b = matr('a'), matr('b')
+        a, oper_list = rref(a, opers=True)
+        for i in oper_list:
+            b = mul_matrix(i, b)
+        return matrix(a, b, cls="Augment")
+
+    oper_list = []
+    lenth = min(matr('row'), matr('column'))
+    if display:
+        print_matrix(matr, end='\n')
+
+    for i in range(lenth):
+        for j in range(matr('row')):
+            if i == j:continue
+
+            elif matr(i, i) == 0:
+                for k in range(i+1, matr("row")):
+                    matr, oper = (swap_row(matr, i, k))
+                    oper_list.append(oper)
+                    if matr(i, i) != 0: break
+                if  k is None or (k == matr("row") - 1): continue
+
+            matr, oper = (add_row(matr, -(matr(j, i)/matr(i, i)), i, j))
+            oper_list.append(oper)
+            if display:
+                print_matrix(matr, '\n')
+
+        for i in range(lenth):
+            if matr(i, i) != 0:
+                matr, oper = (add_row(matr, (1/matr(i, i)) - 1, i, i))
+                oper_list.append(oper)
+    return matr if not operation else (matr, oper_list)
+
+
 
 # test
 row1 = [1, 0]
@@ -280,3 +314,6 @@ tran     = transfer
 E_matr   = identity_matrix
 reverse  = reverse_matrix
 kth      = kth_matrix
+
+a = tran(a)
+d = matrix(a, b, cls="Augment")
